819a8c3959f62927ef6f211493a397abcb111edf
unknown <leila.hadjioual@gmail.com>
2020-02-07 15:35:28 +0100

    add pages and tabbar

---
+++ src/app/awards-list/awards-list.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-awards-list',
+  templateUrl: './awards-list.page.html',
+  styleUrls: ['./awards-list.page.scss'],
+})
+export class AwardsListPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
