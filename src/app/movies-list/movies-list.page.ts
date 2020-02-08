ae5b2d68b7dd18e49b3f425db839c5ad1b172f7d
yohan <yohan.madeira@gmail.com>
2020-02-08 17:52:02 +0100

    Add function to pass argument and changing page

--- src/app/movies-list/movies-list.page.ts
+++ src/app/movies-list/movies-list.page.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProjectInterface} from "../project-interface";
-import { ProjetService } from "../../Service/projet.service";
+import {ProjetService} from "../../Service/projet.service";
+import {Router} from "@angular/router";
+import {DataService} from "../../Service/data.service";
 
 @Component({
-  selector: 'app-movies-list',
-  templateUrl: './movies-list.page.html',
-  styleUrls: ['./movies-list.page.scss'],
+    selector: 'app-movies-list',
+    templateUrl: './movies-list.page.html',
+    styleUrls: ['./movies-list.page.scss'],
 })
 export class MoviesListPage implements OnInit {
 
-  projectInterface: ProjectInterface[] = [];
+    projectInterface: ProjectInterface[] = [];
 
-  constructor(private projectService: ProjetService) { }
+    constructor(private projectService: ProjetService, private router: Router, private data: DataService) {
+    }
 
-  ngOnInit() {
-    this.projectService.get().subscribe(data =>{
-      console.log(data);
-      this.projectInterface = data['hydra:member'];
-      console.log(this.projectInterface);
-    });
-  }
+    ngOnInit() {
+        this.projectService.get().subscribe(data => {
+            this.projectInterface = data['hydra:member'];
+        });
+    }
+
+    ChangeToMovieDetails(pathName: String, info: ProjectInterface) {
+        this.data.setParams(info);
+        this.router.navigate([pathName]);
+    }
 
 }
